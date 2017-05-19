1ae967ebcf9e762b76734ab0288c98d12217f7e3
ysallen0514 <allen861018>
2017-05-19 17:11:37 +0800

    20170519v2

--- homework01.php
+++ homework01.php
@@ -5,17 +5,24 @@
  * Date: 2017/5/19
  * Time: 下午 04:09
  */
-//$x = rand(0,10000);
-$x = 3000;
-echo $x."<br>";
+$x = mt_rand();
+//$x = 300;
+echo "請問西元 $x 年是不是閏年?<br>answer:";
 
 
-if ($x % 400 == 0) {
-    echo '是閏年';
-} elseif ($x % 100 == 0) {
-    echo '不是閏年';
-} elseif ($x % 4 == 0) {
+if ($x % 400== 0 xor $x % 100 == 0 xor $x % 4== 0) {
     echo '是閏年';
 }else{
     echo '不是閏年';
 }
+
+
+//if ($x % 400 == 0) {
+//    echo '是閏年';
+//} elseif ($x % 100 == 0) {
+//    echo '不是閏年';
+//} elseif ($x % 4 == 0) {
+//    echo '是閏年';
+//}else{
+//    echo '不是閏年';
+//}
